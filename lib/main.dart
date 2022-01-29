cb5ac8038193e2ee5b4bc0ca11247cdea8469c29
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-01-28 21:20:43 -0300

    Basic plugins added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,21 @@
 import 'package:flutter/material.dart';
 
-void main(){}
+void main() {
+  runApp(const MaterialApp(
+    home: InitializePage(),
+  ));
+}
+
+class InitializePage extends StatelessWidget {
+  const InitializePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pedidos'),
+        centerTitle: true,
+      ),
+    );
+  }
+}
