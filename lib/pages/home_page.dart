0143fa3a0d31ce95fffff0fb7bbb47e414069e81
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-02-01 13:52:15 -0300

    home_page.dart PageView physics neverScroll

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -16,6 +16,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return PageView(
+      physics: const NeverScrollableScrollPhysics(),
       controller: _pageController,
       children: [
         Scaffold(
