dcdd2b27ea8b160d71b03d3c37bcedf70f6b5d77
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-02-10 17:06:49 -0300

    Future Builder added to get realtime orders from firebase

--- lib/pages/order/order_list_page.dart
+++ lib/pages/order/order_list_page.dart
@@ -4,6 +4,8 @@ import 'package:controle_pedidos/pages/order/order_registration_page.dart';
 import 'package:controle_pedidos/widgets/custom_drawer.dart';
 import 'package:controle_pedidos/widgets/tiles/order_list_tile.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:scoped_model/scoped_model.dart';
 
 class OrderListPage extends StatefulWidget {
   const OrderListPage({Key? key}) : super(key: key);
@@ -23,10 +25,16 @@ class _OrderListPageState extends State<OrderListPage> {
 
   @override
   Widget build(BuildContext context) {
-
     void _showOrderRegistrationPage({OrderData? order}) async {
-      final recOrder = await Navigator.push(context, MaterialPageRoute(builder: (context) => OrderRegistrationPage()));
-      if (recOrder != null){
+      final recOrder = await Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => order == null
+                  ? OrderRegistrationPage()
+                  : OrderRegistrationPage(
+                      order: order,
+                    )));
+      if (recOrder != null) {
         _setOrderList();
       }
     }
@@ -38,21 +46,61 @@ class _OrderListPageState extends State<OrderListPage> {
       ),
       drawer: const CustomDrawer(),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){
+        onPressed: () {
           _showOrderRegistrationPage();
         },
         child: const Icon(Icons.add),
       ),
-      body: ListView.builder(
-        itemCount: orderList?.length,
-        itemBuilder: (context, index) {
-          var order = orderList?[index];
-          if (order != null) {
-            return OrderListTile(
-              order: order,
+      body: ScopedModelDescendant<OrderModel>(
+        builder: (context, child, model) {
+          if (model.isLoading) {
+            return const Center(
+              child: CircularProgressIndicator(),
             );
           }
-          return Container();
+          return FutureBuilder(
+            future: _setOrderList(),
+            builder: (context, snapshot) {
+                return ListView.builder(
+                  itemCount: orderList?.length,
+                  itemBuilder: (context, index) {
+                    var order = orderList?[index];
+                    if (order != null) {
+                      return Slidable(
+                        key: const ValueKey(0),
+                        startActionPane: ActionPane(
+                          dismissible: null,
+                          motion: const ScrollMotion(),
+                          children: [
+                            SlidableAction(
+                              onPressed: (e) {},
+                              icon: Icons.delete_forever,
+                              backgroundColor: Colors.red,
+                              label: 'Apagar',
+                            ),
+                            SlidableAction(
+                              onPressed: (e) {
+                                _showOrderRegistrationPage(order: order);
+                              },
+                              icon: Icons.edit,
+                              backgroundColor: Colors.deepPurple,
+                              label: 'Editar',
+                            ),
+                          ],
+                        ),
+                        child: OrderListTile(
+                          order: order,
+                          showOrderRegistrationPage: () {
+                            _showOrderRegistrationPage(order: order);
+                          },
+                        ),
+                      );
+                    }
+                    return Container();
+                  },
+                );
+              }
+          );
         },
       ),
     );
