166fbf23b941fc4b82cfa6916cec22accc534b5f
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-02-09 23:25:49 -0300

    order_data.dart added enabled field

--- lib/data/order_data.dart
+++ lib/data/order_data.dart
@@ -2,12 +2,13 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:controle_pedidos/data/client_data.dart';
 
 class OrderData {
-  OrderData({required this.client, required this.creationDate});
+  OrderData({required this.client, required this.creationDate, required this.enabled});
 
   OrderData.fromDocSnapshot(DocumentSnapshot snapshot) {
     id = snapshot.get('id');
     creationDate = snapshot.get('creationDate');
     client = ClientData.fromMap(snapshot.get('client'));
+    enabled = snapshot.get('enabled');
   }
 
   OrderData.fromMap(Map<String, dynamic> map) {
@@ -19,11 +20,13 @@ class OrderData {
   String? id;
   late DateTime creationDate;
   late ClientData client;
+  late bool enabled;
 
   Map<String, dynamic> toMap() {
     return {
       'id': id,
       'creationDate': creationDate,
+      'enabled': enabled,
       'client': client.toMap(),
     };
   }
