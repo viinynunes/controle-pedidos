6c60cc4879deeb9cea845937bfa2c8972b7ce254
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-02-09 23:25:18 -0300

    order_model.dart created to communicate with firebase

---
+++ lib/model/order_model.dart
@@ -0,0 +1,47 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:controle_pedidos/data/order_data.dart';
+import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
+
+class OrderModel extends Model {
+  final firebaseCollection = FirebaseFirestore.instance.collection('orders');
+
+  bool isLoading = false;
+
+  static OrderModel of(BuildContext context) => ScopedModel.of<OrderModel>(context);
+
+  void createOrder(OrderData order){
+    isLoading = true;
+    firebaseCollection.doc().set(order.toMap());
+    isLoading = false;
+    notifyListeners();
+  }
+
+  void updateOrder(OrderData order){
+    isLoading = true;
+    firebaseCollection.doc(order.id).update(order.toMap());
+    isLoading = false;
+    notifyListeners();
+  }
+
+  void disableOrder(OrderData order){
+    isLoading = true;
+    order.enabled = false;
+    firebaseCollection.doc(order.id).update(order.toMap());
+    isLoading = false;
+    notifyListeners();
+  }
+
+  Future<List<OrderData>> getAllEnabledOrders() async {
+    isLoading = true;
+    List<OrderData> orderList = [];
+    final snapshot = await firebaseCollection.where('enabled', isEqualTo: true).orderBy('creationDate', descending: true).get();
+    for (var e in snapshot.docs){
+      orderList.add(OrderData.fromDocSnapshot(e));
+    }
+
+    isLoading = false;
+    notifyListeners();
+    return orderList;
+  }
+}
