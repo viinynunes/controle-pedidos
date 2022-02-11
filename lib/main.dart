49fb90270d8e8c1c25440859e81a742c65ed7844
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-02-11 17:56:04 -0300

    Removed drawer_page_controller.dart to solve a bug on Drawer and modified all pages that use custom_drawer.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:controle_pedidos/model/client_model.dart';
-import 'package:controle_pedidos/model/drawer_page_controller.dart';
 import 'package:controle_pedidos/model/establishment_model.dart';
 import 'package:controle_pedidos/model/order_model.dart';
 import 'package:controle_pedidos/model/product_model.dart';
@@ -9,8 +8,8 @@ import 'package:controle_pedidos/pages/home_page.dart';
 import 'package:controle_pedidos/pages/login_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:scoped_model/scoped_model.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:scoped_model/scoped_model.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,8 +21,6 @@ void main() async {
         builder: (context, child, model) {
           return ScopedModel<ClientModel>(
             model: ClientModel(),
-            child: ScopedModel<DrawerPageController>(
-              model: DrawerPageController(),
               child: ScopedModel<OrderModel>(
                 model: OrderModel(),
                 child: ScopedModel<ProductModel>(
@@ -60,8 +57,7 @@ void main() async {
                   ),
                 ),
               ),
-            ),
-          );
+            );
         },
       ),
     ),
