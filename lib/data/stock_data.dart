99d21ee7bdddb3289b32da7159cda32b22cb97db
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-02-15 22:49:27 -0300

    Basic entity class created

---
+++ lib/data/stock_data.dart
@@ -0,0 +1,28 @@
+import 'package:controle_pedidos/data/product_data.dart';
+
+class StockData {
+  StockData.fromMap(Map<String, dynamic> map) {
+    id = map['id'];
+    total = map[''];
+    left = map['left'];
+    creationDate = map['creationDate'];
+    product = ProductData.fromMap(map['product']);
+  }
+
+  String? id;
+  late int total;
+  late int left;
+  late DateTime creationDate;
+
+  late ProductData product;
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'total': total,
+      'left': left,
+      'creationDate': creationDate,
+      'product': product.toMap(),
+    };
+  }
+}
