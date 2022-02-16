4a05b87276c9349c20acae3f060885b60ff743ce
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-02-16 16:17:25 -0300

    Added Stock Scoped Model

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:controle_pedidos/model/establishment_model.dart';
 import 'package:controle_pedidos/model/order_model.dart';
 import 'package:controle_pedidos/model/product_model.dart';
 import 'package:controle_pedidos/model/provider_model.dart';
+import 'package:controle_pedidos/model/stock_model.dart';
 import 'package:controle_pedidos/model/user_model.dart';
 import 'package:controle_pedidos/pages/home_page.dart';
 import 'package:controle_pedidos/pages/login_page.dart';
@@ -19,45 +20,48 @@ void main() async {
       model: UserModel(),
       child: ScopedModelDescendant<UserModel>(
         builder: (context, child, model) {
-          return ScopedModel<ClientModel>(
-            model: ClientModel(),
-              child: ScopedModel<OrderModel>(
-                model: OrderModel(),
-                child: ScopedModel<ProductModel>(
-                  model: ProductModel(),
-                  child: ScopedModel<ProviderModel>(
-                    model: ProviderModel(),
-                    child: ScopedModel<EstablishmentModel>(
-                      model: EstablishmentModel(),
-                      child: MaterialApp(
-                        home:
-                            model.isLoggedIn() ? const HomePage() : const LoginPage(),
-                        theme: ThemeData(
-                          primarySwatch: Colors.deepPurple,
-                          primaryColor: Colors.deepPurple,
-                          inputDecorationTheme: const InputDecorationTheme(
-                              border: OutlineInputBorder(
-                                  borderSide: BorderSide(color: Colors.white),
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(16))),
-                              enabledBorder: OutlineInputBorder(
-                                  borderSide: BorderSide(color: Colors.white),
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(16)))),
+          return ScopedModel<StockModel>(
+            model: StockModel(),
+            child: ScopedModel<ClientModel>(
+              model: ClientModel(),
+                child: ScopedModel<OrderModel>(
+                  model: OrderModel(),
+                  child: ScopedModel<ProductModel>(
+                    model: ProductModel(),
+                    child: ScopedModel<ProviderModel>(
+                      model: ProviderModel(),
+                      child: ScopedModel<EstablishmentModel>(
+                        model: EstablishmentModel(),
+                        child: MaterialApp(
+                          home:
+                              model.isLoggedIn() ? const HomePage() : const LoginPage(),
+                          theme: ThemeData(
+                            primarySwatch: Colors.deepPurple,
+                            primaryColor: Colors.deepPurple,
+                            inputDecorationTheme: const InputDecorationTheme(
+                                border: OutlineInputBorder(
+                                    borderSide: BorderSide(color: Colors.white),
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(16))),
+                                enabledBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(color: Colors.white),
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(16)))),
+                          ),
+                          localizationsDelegates: const [
+                            GlobalMaterialLocalizations.delegate,
+                            GlobalWidgetsLocalizations.delegate
+                          ],
+                          supportedLocales: const [
+                            Locale('pt')
+                          ],
                         ),
-                        localizationsDelegates: const [
-                          GlobalMaterialLocalizations.delegate,
-                          GlobalWidgetsLocalizations.delegate
-                        ],
-                        supportedLocales: const [
-                          Locale('pt')
-                        ],
                       ),
                     ),
                   ),
                 ),
               ),
-            );
+          );
         },
       ),
     ),
