f81ea99885550e6cafff4073e231c855d8be7973
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-02-21 22:54:06 -0300

    Wrapped Column with Widget to Image to convert to image file

--- lib/pages/reports/orders/report_one_order_page.dart
+++ lib/pages/reports/orders/report_one_order_page.dart
@@ -1,62 +1,80 @@
 import 'package:controle_pedidos/data/order_data.dart';
+import 'package:controle_pedidos/utils/transform_widget_to_image.dart';
+import 'package:controle_pedidos/utils/widget_to_image.dart';
 import 'package:flutter/material.dart';
 
-class ReportOneOrderPage extends StatelessWidget {
+
+class ReportOneOrderPage extends StatefulWidget {
   const ReportOneOrderPage({Key? key, required this.order}) : super(key: key);
 
   final OrderData order;
 
   @override
-  Widget build(BuildContext context) {
+  _ReportOneOrderPageState createState() => _ReportOneOrderPageState();
+}
 
-    final _widgetKey = GlobalKey();
+class _ReportOneOrderPageState extends State<ReportOneOrderPage> {
+
+  late GlobalKey key1;
+
+  @override
+  Widget build(BuildContext context) {
 
     return Scaffold(
         appBar: AppBar(
-          title: Text(order.client.name),
+          title: Text(widget.order.client.name),
           centerTitle: true,
           actions: [
             IconButton(
-              onPressed: () {},
+              onPressed: () async {
+                await TransformWidgetToImage.getWidget(key1, widget.order.client.name);
+              },
               icon: const Icon(Icons.share),
             ),
           ],
         ),
-        body: Column(
-          key: _widgetKey,
-          children: [
-            const SizedBox(height: 20,),
-            Center(
-              child: Text(
-                order.client.name,
-                style: const TextStyle(fontSize: 20),
+        body: WidgetToImage(
+          builder: (key) {
+            key1 = key;
+            return Container(
+              color: Colors.white,
+              child: Column(
+                children: [
+                  const SizedBox(height: 20,),
+                  Center(
+                    child: Text(
+                      widget.order.client.name,
+                      style: const TextStyle(fontSize: 20),
+                    ),
+                  ),
+                  const SizedBox(height: 10,),
+                  Flexible(
+                    child: ListView.builder(
+                      itemCount: widget.order.orderItemList!.length,
+                      itemBuilder: (context, index) {
+                        var item = widget.order.orderItemList![index];
+                        String note = item.note ?? '';
+                        return Row(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8),
+                              child: Text(item.quantity.toString() +
+                                  ' ' +
+                                  item.product.category +
+                                  ' ' +
+                                  item.product.name +
+                                  ' - ' +
+                                  note, style: const TextStyle(fontSize: 16),),
+                            ),
+                          ],
+                        );
+                      },
+                    ),
+                  )
+                ],
               ),
-            ),
-            const SizedBox(height: 10,),
-            Flexible(
-              child: ListView.builder(
-                itemCount: order.orderItemList!.length,
-                itemBuilder: (context, index) {
-                  var item = order.orderItemList![index];
-                  String note = item.note ?? '';
-                  return Row(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(8),
-                        child: Text(item.quantity.toString() +
-                            ' ' +
-                            item.product.category +
-                            ' ' +
-                            item.product.name +
-                            ' - ' +
-                            note, style: const TextStyle(fontSize: 16),),
-                      ),
-                    ],
-                  );
-                },
-              ),
-            )
-          ],
+            );
+          },
         ));
   }
 }
