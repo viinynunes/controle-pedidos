f1385f7c2c1e2bfc13ba1b5895f7dc7dc120793f
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-02-24 22:51:27 -0300

    Changed Pixel Ratio

--- lib/utils/transform_widget_to_image.dart
+++ lib/utils/transform_widget_to_image.dart
@@ -12,7 +12,7 @@ class TransformWidgetToImage {
     RenderRepaintBoundary boundary =
         key.currentContext?.findRenderObject() as RenderRepaintBoundary;
 
-    final image = await boundary.toImage(pixelRatio: 1);
+    final image = await boundary.toImage(pixelRatio: 3);
     final byteData = await image.toByteData(format: ui.ImageByteFormat.png);
     final pngBytes = byteData?.buffer.asUint8List();
 
