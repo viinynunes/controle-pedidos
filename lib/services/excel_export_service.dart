ece2c7c88e4e4d8fa72a42df4374db6afbb060de
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-02-25 17:40:06 -0300

    Method createAndOpenExcelToEstablishment formats the list of stock data to Excel file

--- lib/services/excel_export_service.dart
+++ lib/services/excel_export_service.dart
@@ -1,11 +1,13 @@
 import 'dart:io';
 
+import 'package:controle_pedidos/data/provider_data.dart';
 import 'package:intl/intl.dart';
 import 'package:open_file/open_file.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:syncfusion_flutter_xlsio/xlsio.dart';
 
 import '../data/order_data.dart';
+import '../data/stock_data.dart';
 
 class ExcelExportService {
   final dateFormat = DateFormat('dd-MM-yyyy');
@@ -52,6 +54,68 @@ class ExcelExportService {
     workbook.dispose();
 
     _saveAndOpenFile(bytes, 'pedidos ${dateFormat.format(DateTime.now())}');
+  }
+
+  void createAndOpenExcelToEstablishment(List<StockData> stockList) async {
+    final workbook = Workbook();
+    final sheet = workbook.worksheets[0];
+
+    int rowIndex = 1, columnIndex = 1, columnInitValue = 1;
+    ProviderData? lastProvider;
+
+    bool firstLine = true;
+
+    for (var stockItem in stockList) {
+      rowIndex++;
+      columnIndex = columnInitValue;
+
+      if(firstLine){
+        sheet.getRangeByIndex(rowIndex, columnIndex).setText(stockItem.product.provider.name);
+        rowIndex++;
+        firstLine = false;
+      }
+
+      lastProvider ??= stockItem.product.provider;
+      if (lastProvider != stockItem.product.provider) {
+        rowIndex++;
+        lastProvider = stockItem.product.provider;
+        sheet.getRangeByIndex(rowIndex, columnIndex).setText(lastProvider.name);
+        rowIndex++;
+      }
+      sheet
+          .getRangeByIndex(rowIndex, columnIndex)
+          .setText(stockItem.product.name);
+
+      columnIndex++;
+
+      sheet.getRangeByIndex(rowIndex, columnIndex).setText(
+          stockItem.product.category +
+              '   ' +
+              stockItem.total.toString());
+
+      columnIndex++;
+      sheet.getRangeByIndex(rowIndex, columnIndex).setText(
+          stockItem.product.category +
+              '   ' +
+              stockItem.totalOrdered.toString());
+
+      columnIndex++;
+      sheet
+          .getRangeByIndex(rowIndex, columnIndex)
+          .setText((stockItem.totalOrdered - stockItem.total).toString());
+
+      sheet.autoFitColumn(columnInitValue);
+      if (rowIndex > 11) {
+        rowIndex = 1;
+        columnInitValue = columnInitValue + 5;
+      }
+    }
+
+    final List<int> bytes = workbook.saveAsStream();
+    workbook.dispose();
+
+    _saveAndOpenFile(
+        bytes, 'Relatório Final ${dateFormat.format(DateTime.now())}');
   }
 
   void _saveAndOpenFile(List<int> bytes, String name) async {
