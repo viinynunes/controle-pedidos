661e07601e54c708cae5a970e0e401c4b380bd66
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-02-26 14:17:22 -0300

    addEmptyProductInStock receives a product and if it isn't in stock list by date, add the stock with 0 total

---
+++ lib/services/control_service.dart
@@ -0,0 +1,18 @@
+import 'package:controle_pedidos/data/product_data.dart';
+import 'package:controle_pedidos/data/stock_data.dart';
+import 'package:controle_pedidos/model/stock_model.dart';
+import 'package:flutter/material.dart';
+
+class ControlService {
+  void addEmptyProductInStock(ProductData product, BuildContext context,
+      DateTime iniDate, DateTime endDate) async {
+    final list =
+        await StockModel.of(context).getStockBetweenDates(iniDate, endDate);
+
+    StockData stockData = StockData(0, 0, DateTime.now(), product);
+
+    if (!list.contains(stockData)) {
+      await StockModel.of(context).createStockItem(stockData);
+    }
+  }
+}
