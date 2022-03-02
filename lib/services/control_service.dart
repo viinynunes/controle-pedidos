8cf6ecefdaa6617c6abfdf47cd16ce3a394ae0fc
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-03-02 17:15:59 -0300

    Method loadEmptyProductsListInStock insert in stock the products that have the var stockDefault equal true into stock.

--- lib/services/control_service.dart
+++ lib/services/control_service.dart
@@ -1,5 +1,6 @@
 import 'package:controle_pedidos/data/product_data.dart';
 import 'package:controle_pedidos/data/stock_data.dart';
+import 'package:controle_pedidos/model/product_model.dart';
 import 'package:controle_pedidos/model/stock_model.dart';
 import 'package:flutter/material.dart';
 
@@ -13,6 +14,17 @@ class ControlService {
 
     if (!list.contains(stockData)) {
       await StockModel.of(context).createStockItem(stockData);
+    }
+  }
+
+  Future<void> loadEmptyProductsListInStock(BuildContext context) async {
+    final list = await ProductModel.of(context).getEnabledProductsByStockDefaultTrue();
+
+    for (var product in list){
+
+      StockData newStock = StockData(0, 0, DateTime.now(), product);
+
+      await StockModel.of(context).createStockItem(newStock);
     }
   }
 }
