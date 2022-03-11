e385cc90735ee5027836688d032e3013f09da26d
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-03-11 18:04:40 -0300

    Added bottomNavigationBarColor

--- lib/utils/custom_colors.dart
+++ lib/utils/custom_colors.dart
@@ -5,4 +5,5 @@ class CustomColors {
   static const backgroundColor = Color(0xFF202124);
   static const backgroundTile = Color(0xFF2D2D31);
   static const textColorTile = Color(0xFFF0F2F4);
+  static const bottomNavigationBarColor = Color(0xFF141517);
 }
