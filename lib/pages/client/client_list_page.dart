ac7d7d11331994062d41564a5790a37c2b5163bc
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-03-11 18:10:06 -0300

    Removed dependency from model and Future Builder

--- lib/pages/client/client_list_page.dart
+++ lib/pages/client/client_list_page.dart
@@ -3,7 +3,6 @@ import 'package:controle_pedidos/model/client_model.dart';
 import 'package:controle_pedidos/widgets/tiles/client_list_tile.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
-import 'package:scoped_model/scoped_model.dart';
 
 import 'client_registration_page.dart';
 
@@ -20,6 +19,15 @@ class ClientListPage extends StatefulWidget {
 }
 
 class _ClientListPageState extends State<ClientListPage> {
+  List<ClientData> clientList = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    _getClientList();
+  }
+
   void _showClientRegistrationPage(ClientData? client) async {
     Navigator.push(
         context,
@@ -31,61 +39,47 @@ class _ClientListPageState extends State<ClientListPage> {
 
   @override
   Widget build(BuildContext context) {
-    return ScopedModelDescendant<ClientModel>(
-      builder: (context, child, model) {
-        if (model.isLoading) {
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        }
-        return FutureBuilder<List<ClientData>>(
-          future: model.getFilteredClients(search: widget.search),
-          builder: (context, snapshot) {
-            if (!snapshot.hasData) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            } else {
-              return ListView.builder(
-                itemCount: snapshot.data!.length,
-                itemBuilder: (context, index) {
-                  var item = snapshot.data![index];
-                  return Slidable(
-                    key: const ValueKey(0),
-                    startActionPane: ActionPane(
-                      motion: const ScrollMotion(),
-                      dismissible: null,
-                      children: [
-                        SlidableAction(
-                          onPressed: (e) {
-                            setState(() {
-                              model.disableClient(snapshot.data![index]);
-                            });
-                          },
-                          icon: Icons.delete_forever,
-                          label: 'Apagar',
-                          backgroundColor: Colors.red,
-                        ),
-                        SlidableAction(
-                          onPressed: (e) {
-                            setState(() {
-                              _showClientRegistrationPage(item);
-                            });
-                          },
-                          icon: Icons.edit,
-                          label: 'Editar',
-                          backgroundColor: Colors.deepPurple,
-                        )
-                      ],
-                    ),
-                    child: ClientListTile(client: item),
-                  );
+    return ListView.builder(
+      itemCount: clientList.length,
+      itemBuilder: (context, index) {
+        var client = clientList[index];
+        return Slidable(
+          key: const ValueKey(0),
+          startActionPane: ActionPane(
+            motion: const ScrollMotion(),
+            dismissible: null,
+            children: [
+              SlidableAction(
+                onPressed: (e) {
+                  setState(() {});
                 },
-              );
-            }
-          },
+                icon: Icons.delete_forever,
+                label: 'Apagar',
+                backgroundColor: Colors.red,
+              ),
+              SlidableAction(
+                onPressed: (e) {
+                  setState(() {
+                    _showClientRegistrationPage(client);
+                  });
+                },
+                icon: Icons.edit,
+                label: 'Editar',
+                backgroundColor: Colors.deepPurple,
+              )
+            ],
+          ),
+          child: ClientListTile(client: client),
         );
       },
     );
+  }
+
+  Future<void> _getClientList() async {
+    final list = await ClientModel.of(context).getFilteredClients();
+
+    setState(() {
+      clientList = list;
+    });
   }
 }
