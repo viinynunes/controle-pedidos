271dade907b30e9e7164f32d02d33066968cd4eb
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-03-12 14:59:16 -0300

    Created registration_home_page.dart that's handle with bottom navigation bar on Home Page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,7 @@
-import 'package:controle_pedidos/pages/client/client_list_page.dart';
 import 'package:controle_pedidos/pages/control/control_home_page.dart';
-import 'package:controle_pedidos/pages/establishment/establishment_list_page.dart';
 import 'package:controle_pedidos/pages/order/order_list_page.dart';
-import 'package:controle_pedidos/pages/product/product_list_page.dart';
-import 'package:controle_pedidos/pages/provider/provider_list_page.dart';
+import 'package:controle_pedidos/pages/registration/registration_home_page.dart';
 import 'package:controle_pedidos/pages/reports/report_home.dart';
-import 'package:controle_pedidos/utils/custom_colors.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -16,26 +12,10 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _registrationPageIndex = 0;
   final _pageController = PageController();
 
   @override
   Widget build(BuildContext context) {
-    final List<Widget> _registrationPageElements = <Widget>[
-      ProductListPage(
-        pageController: _pageController,
-      ),
-      ClientListPage(
-        pageController: _pageController,
-      ),
-      ProviderListPage(
-        pageController: _pageController,
-      ),
-      EstablishmentListPage(
-        pageController: _pageController,
-      ),
-    ];
-
     return PageView(
       physics: const NeverScrollableScrollPhysics(),
       controller: _pageController,
@@ -44,47 +24,7 @@ class _HomePageState extends State<HomePage> {
           pageController: _pageController,
         ),
         ControlHomePage(pageController: _pageController),
-        Scaffold(
-            body: _registrationPageElements.elementAt(_registrationPageIndex),
-            bottomNavigationBar: BottomNavigationBar(
-              type: BottomNavigationBarType.fixed,
-              backgroundColor: CustomColors.bottomNavigationBarColor,
-              selectedItemColor: Colors.deepPurple,
-              unselectedItemColor: CustomColors.textColorTile,
-              currentIndex: _registrationPageIndex,
-              elevation: 10,
-              items: const [
-                BottomNavigationBarItem(
-                    icon: Icon(
-                      Icons.production_quantity_limits,
-                      color: CustomColors.textColorTile,
-                    ),
-                    label: 'Produtos'),
-                BottomNavigationBarItem(
-                    icon: Icon(
-                      Icons.account_circle_outlined,
-                      color: CustomColors.textColorTile,
-                    ),
-                    label: 'Clientes'),
-                BottomNavigationBarItem(
-                    icon: Icon(
-                      Icons.sports_handball_outlined,
-                      color: CustomColors.textColorTile,
-                    ),
-                    label: 'Fornecedores'),
-                BottomNavigationBarItem(
-                    icon: Icon(
-                      Icons.account_balance_sharp,
-                      color: CustomColors.textColorTile,
-                    ),
-                    label: 'Estabelecimentos'),
-              ],
-              onTap: (index) {
-                setState(() {
-                  _registrationPageIndex = index;
-                });
-              },
-            )),
+        RegistrationHomePage(pageController: _pageController),
         ReportHomePage(
           pageController: _pageController,
         ),
