d725654c1be1e75f4a1ac5ede89999e4fb7ccd69
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-03-12 16:54:31 -0300

    Implemented sortProviderListByEstablishmentAndRegistration

--- lib/services/provider_service.dart
+++ lib/services/provider_service.dart
@@ -5,4 +5,17 @@ class ProviderService {
     providerList
         .sort((a, b) => a.name.toLowerCase().compareTo(b.name.toLowerCase()));
   }
+
+  void sortProviderListByEstablishmentAndRegistration(List<ProviderData> providerList) {
+    providerList.sort((a, b) {
+      int compare = a.establishment.registrationDate.compareTo(b.establishment.registrationDate);
+
+      if (compare == 0){
+        return a.registrationDate.compareTo(b.registrationDate);
+      } else {
+        return compare;
+      }
+    });
+  }
+
 }
