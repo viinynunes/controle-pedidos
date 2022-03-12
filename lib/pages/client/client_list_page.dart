363c1ac909e1673168201391bfa6d52fd571d0e2
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-03-12 14:19:23 -0300

    Create and update client working

--- lib/pages/client/client_list_page.dart
+++ lib/pages/client/client_list_page.dart
@@ -1,25 +1,29 @@
 import 'package:controle_pedidos/data/client_data.dart';
 import 'package:controle_pedidos/model/client_model.dart';
+import 'package:controle_pedidos/services/client_service.dart';
 import 'package:controle_pedidos/widgets/tiles/client_list_tile.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 
+import '../../widgets/custom_drawer.dart';
 import 'client_registration_page.dart';
 
 class ClientListPage extends StatefulWidget {
   const ClientListPage({
     Key? key,
-    this.search,
+    required this.pageController,
   }) : super(key: key);
 
-  final String? search;
+  final PageController pageController;
 
   @override
   _ClientListPageState createState() => _ClientListPageState();
 }
 
 class _ClientListPageState extends State<ClientListPage> {
+  final service = ClientService();
   List<ClientData> clientList = [];
+  List<ClientData> secondaryClientList = [];
 
   @override
   void initState() {
@@ -28,51 +32,95 @@ class _ClientListPageState extends State<ClientListPage> {
     _getClientList();
   }
 
-  void _showClientRegistrationPage(ClientData? client) async {
-    Navigator.push(
+  void _showClientRegistrationPage({ClientData? client}) async {
+    final recClient = await Navigator.push(
         context,
         MaterialPageRoute(
             builder: (context) => ClientRegistrationPage(
                   client: client,
                 )));
+
+    if (recClient != null) {
+      if (client != null) {
+        setState(() {
+          ClientModel.of(context).updateClient(recClient);
+          secondaryClientList.remove(client);
+        });
+      } else {
+        ClientModel.of(context).createClient(recClient);
+      }
+      setState(() {
+        secondaryClientList.add(recClient);
+      });
+    }
+
+    setState(() {
+      service.sortClientsByName(secondaryClientList);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: clientList.length,
-      itemBuilder: (context, index) {
-        var client = clientList[index];
-        return Slidable(
-          key: const ValueKey(0),
-          startActionPane: ActionPane(
-            motion: const ScrollMotion(),
-            dismissible: null,
-            children: [
-              SlidableAction(
-                onPressed: (e) {
-                  setState(() {});
-                },
-                icon: Icons.delete_forever,
-                label: 'Apagar',
-                backgroundColor: Colors.red,
-              ),
-              SlidableAction(
-                onPressed: (e) {
-                  setState(() {
-                    _showClientRegistrationPage(client);
-                  });
-                },
-                icon: Icons.edit,
-                label: 'Editar',
-                backgroundColor: Colors.deepPurple,
-              )
-            ],
+    return Scaffold(
+        appBar: AppBar(
+          title: TextField(
+            decoration: const InputDecoration(
+                enabledBorder: InputBorder.none,
+                hintText: 'Pesquisar',
+                hintStyle: TextStyle(color: Colors.white)),
+            style: const TextStyle(color: Colors.white, fontSize: 22),
+            onChanged: (text) async {
+              _filterClients(text);
+            },
           ),
-          child: ClientListTile(client: client),
-        );
-      },
-    );
+          centerTitle: true,
+        ),
+        drawer: CustomDrawer(
+          pageController: widget.pageController,
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            _showClientRegistrationPage();
+          },
+          child: const Icon(
+            Icons.add,
+            size: 40,
+          ),
+        ),
+        body: ListView.builder(
+          itemCount: secondaryClientList.length,
+          itemBuilder: (context, index) {
+            var client = secondaryClientList[index];
+            return Slidable(
+              key: const ValueKey(0),
+              startActionPane: ActionPane(
+                motion: const ScrollMotion(),
+                dismissible: null,
+                children: [
+                  SlidableAction(
+                    onPressed: (e) {
+                      setState(() {});
+                    },
+                    icon: Icons.delete_forever,
+                    label: 'Apagar',
+                    backgroundColor: Colors.red,
+                  ),
+                  SlidableAction(
+                    onPressed: (e) {
+                      setState(() {
+                        _showClientRegistrationPage(client: client);
+                      });
+                    },
+                    icon: Icons.edit,
+                    label: 'Editar',
+                    backgroundColor: Colors.deepPurple,
+                  )
+                ],
+              ),
+              child: ClientListTile(client: client),
+            );
+          },
+        ));
   }
 
   Future<void> _getClientList() async {
@@ -80,6 +128,32 @@ class _ClientListPageState extends State<ClientListPage> {
 
     setState(() {
       clientList = list;
+      secondaryClientList.addAll(clientList);
     });
+  }
+
+  void _filterClients(String search) {
+    List<ClientData> changeList = [];
+    changeList.addAll(clientList);
+
+    if (search.isNotEmpty) {
+      List<ClientData> filteredList = [];
+      for (var client in changeList) {
+        if (client.name.toLowerCase().contains(search.toLowerCase())) {
+          filteredList.add(client);
+        }
+      }
+
+      setState(() {
+        secondaryClientList.clear();
+        secondaryClientList.addAll(filteredList);
+      });
+      return;
+    } else {
+      setState(() {
+        secondaryClientList.clear();
+        secondaryClientList.addAll(clientList);
+      });
+    }
   }
 }
