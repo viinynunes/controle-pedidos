93a116d46b457b88f107be317d558687ecaec734
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-03-12 13:37:03 -0300

    Removed InkWell and onTap function

--- lib/widgets/tiles/client_list_tile.dart
+++ lib/widgets/tiles/client_list_tile.dart
@@ -1,5 +1,4 @@
 import 'package:controle_pedidos/data/client_data.dart';
-import 'package:controle_pedidos/pages/client/client_registration_page.dart';
 import 'package:flutter/material.dart';
 
 class ClientListTile extends StatefulWidget {
@@ -13,40 +12,26 @@ class ClientListTile extends StatefulWidget {
 
 class _ClientListTileState extends State<ClientListTile> {
 
-  void _showClientRegistrationPage(ClientData? client) async {
-    Navigator.push(
-        context,
-        MaterialPageRoute(
-            builder: (context) => ClientRegistrationPage(
-              client: client,
-            )));
-  }
-
   @override
   Widget build(BuildContext context) {
     ClientData client = widget.client;
-    return InkWell(
-      onTap: () {
-        _showClientRegistrationPage(client);
-      },
-      child: Card(
-          child: SizedBox(
-        height: 80,
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Row(
-            children: [
-              Expanded(
-                child: Text(
-                  client.name,
-                  style: const TextStyle(fontSize: 20),
-                ),
+    return Card(
+        child: SizedBox(
+      height: 80,
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Row(
+          children: [
+            Expanded(
+              child: Text(
+                client.name,
+                style: const TextStyle(fontSize: 20),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
-      )),
-    );
+      ),
+    ));
   }
 
 
