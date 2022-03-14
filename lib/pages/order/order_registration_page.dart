e8125944ef343bd27fdf23425331f1748f858b6b
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-03-14 15:40:08 -0300

    Creating and updating product from order registration page

--- lib/pages/order/order_registration_page.dart
+++ lib/pages/order/order_registration_page.dart
@@ -7,6 +7,7 @@ import 'package:controle_pedidos/model/order_model.dart';
 import 'package:controle_pedidos/model/product_model.dart';
 import 'package:controle_pedidos/pages/product/product_registration_page.dart';
 import 'package:controle_pedidos/pages/product/showProductListDialog.dart';
+import 'package:controle_pedidos/services/product_service.dart';
 import 'package:controle_pedidos/utils/custom_colors.dart';
 import 'package:controle_pedidos/widgets/tiles/order_item_tile.dart';
 import 'package:dropdown_search/dropdown_search.dart';
@@ -100,7 +101,7 @@ class _OrderRegistrationPageState extends State<OrderRegistrationPage> {
   @override
   Widget build(BuildContext context) {
     void _showProductRegistrationPage({ProductData? product}) async {
-      await Navigator.push(
+      final recProduct = await Navigator.push(
         context,
         MaterialPageRoute(
           builder: (context) => ProductRegistrationPage(
@@ -108,6 +109,33 @@ class _OrderRegistrationPageState extends State<OrderRegistrationPage> {
           ),
         ),
       );
+
+      setState(() {
+        loading = true;
+      });
+
+      if (recProduct != null) {
+        if (product != null) {
+          setState(() {
+            ProductModel.of(context).updateProduct(recProduct);
+            productList.remove(product);
+          });
+
+        } else {
+          ProductModel.of(context).createProduct(recProduct);
+        }
+        setState(() {
+          productList.add(recProduct);
+          _selectedProduct = recProduct;
+        });
+      }
+
+      final productService = ProductService();
+      productService.sortProductsByName(productList);
+
+      setState(() {
+        loading = false;
+      });
       await _setProductList();
     }
 
