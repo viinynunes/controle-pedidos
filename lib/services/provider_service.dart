4d03814e5a3ae9344ce9b152364fd9c729eb2bf9
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-03-16 16:43:00 -0300

    Create or Update method created

--- lib/services/provider_service.dart
+++ lib/services/provider_service.dart
@@ -1,21 +1,53 @@
+import 'package:flutter/material.dart';
+
 import '../data/provider_data.dart';
+import '../model/provider_model.dart';
+import '../pages/provider/provider_registration_page.dart';
 
 class ProviderService {
+  Future<ProviderData> createOrUpdate(
+      {ProviderData? provider,
+      required List<ProviderData> providerList,
+      required BuildContext context}) async {
+
+    final recProv = await Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => ProviderRegistrationPage(
+                  provider: provider,
+                )));
+
+    if (recProv != null) {
+      if (provider != null) {
+        await ProviderModel.of(context).updateProvider(recProv);
+
+        providerList.remove(provider);
+        providerList.add(recProv);
+      } else {
+        ProviderModel.of(context).createProvider(recProv);
+        providerList.add(recProv);
+      }
+    }
+
+    return recProv;
+  }
+
   void sortProviderListByName(List<ProviderData> providerList) {
     providerList
         .sort((a, b) => a.name.toLowerCase().compareTo(b.name.toLowerCase()));
   }
 
-  void sortProviderListByEstablishmentAndRegistration(List<ProviderData> providerList) {
+  void sortProviderListByEstablishmentAndRegistration(
+      List<ProviderData> providerList) {
     providerList.sort((a, b) {
-      int compare = a.establishment.registrationDate.compareTo(b.establishment.registrationDate);
+      int compare = a.establishment.registrationDate
+          .compareTo(b.establishment.registrationDate);
 
-      if (compare == 0){
+      if (compare == 0) {
         return a.registrationDate.compareTo(b.registrationDate);
       } else {
         return compare;
       }
     });
   }
-
 }
