d028d926135db0a9c4e74615fcb9006530b1b287
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-04-19 17:07:10 -0300

    When updating provider, update every product and stock that have the provider

--- lib/model/provider_model.dart
+++ lib/model/provider_model.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:controle_pedidos/data/product_data.dart';
 import 'package:controle_pedidos/data/provider_data.dart';
+import 'package:controle_pedidos/data/stock_data.dart';
 import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
 
@@ -23,7 +25,36 @@ class ProviderModel extends Model {
 
   Future<void> updateProvider(ProviderData provider) async {
     isLoading = true;
+
     await firebaseCollection.doc(provider.id).update(provider.toMap());
+
+    final snapProd = await FirebaseFirestore.instance
+        .collection('products')
+        .where('provider.id', isEqualTo: provider.id)
+        .get();
+    for (var p in snapProd.docs) {
+      final product = ProductData.fromDocSnapshot(p);
+      product.provider = provider;
+      FirebaseFirestore.instance
+          .collection('products')
+          .doc(product.id)
+          .update(product.toMap());
+    }
+
+    final snapStock = await FirebaseFirestore.instance
+        .collection('stock')
+        .where('product.provider.id', isEqualTo: provider.id)
+        .get();
+
+    for (var s in snapStock.docs) {
+      final stock = StockData.fromMap(s.id, s.data());
+      stock.product.provider = provider;
+      FirebaseFirestore.instance
+          .collection('stock')
+          .doc(stock.id)
+          .update(stock.toMap());
+    }
+
     isLoading = false;
     notifyListeners();
   }
