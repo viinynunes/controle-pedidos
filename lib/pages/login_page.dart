4379193c432ecd4460ac13972076936f7c5216b4
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-04-22 17:18:23 -0300

    AutoFills on login page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -38,106 +38,110 @@ class _LoginPageState extends State<LoginPage> {
                         maxWidth: desktop ? 720 : double.maxFinite),
                     child: Form(
                       key: _formKey,
-                      child: ListView(
-                        padding: const EdgeInsets.all(16),
-                        children: [
-                          SizedBox(
-                            height: desktop ? 150 : 100,
-                          ),
-                          Image.asset('assets/icon.png',
-                              height: desktop ? 120 : 100),
-                          const SizedBox(
-                            height: 15,
-                          ),
-                          const Text(
-                            'Controle de Pedidos',
-                            style: TextStyle(fontSize: 30, color: Colors.white),
-                            textAlign: TextAlign.center,
-                          ),
-
-                          /*const Icon(
-                            Icons.water_damage_outlined,
-                            size: 150,
-                            color: Colors.white,
-                          ),*/
-
-                          const SizedBox(
-                            height: 15,
-                          ),
-                          TextFormField(
-                            controller: _emailController,
-                            decoration: const InputDecoration(
-                              hintText: 'Email',
-                              hintStyle: TextStyle(color: Colors.white),
+                      child: AutofillGroup(
+                        child: ListView(
+                          padding: const EdgeInsets.all(16),
+                          children: [
+                            SizedBox(
+                              height: desktop ? 150 : 100,
                             ),
-                            style: const TextStyle(color: Colors.white),
-                            textInputAction: TextInputAction.next,
-                            validator: (text) {
-                              bool regValida = RegExp(
-                                      r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
-                                  .hasMatch(text!);
-                              if (text.isEmpty || !regValida) {
-                                return 'Email Inválido';
-                              }
-                            },
-                          ),
-                          const SizedBox(
-                            height: 30,
-                          ),
-                          TextFormField(
-                            controller: _passwordController,
-                            decoration: const InputDecoration(
-                              hintText: 'Senha',
-                              hintStyle: TextStyle(color: Colors.white),
+                            Image.asset('assets/icon.png',
+                                height: desktop ? 120 : 100),
+                            const SizedBox(
+                              height: 15,
                             ),
-                            style: const TextStyle(color: Colors.white),
-                            obscureText: true,
-                            validator: (text) {
-                              if (text!.isEmpty) {
-                                return 'Preencha o campo';
-                              } else if (text.length < 6) {
-                                return 'A senha deve conter no minimo 6 caracteres';
-                              }
-                            },
-                            onFieldSubmitted: (e) {
-                              if (_formKey.currentState!.validate()) {
-                                model.signIn(
-                                    email: _emailController.text,
-                                    password: _passwordController.text,
-                                    onSuccess: onSuccess,
-                                    onError: onError);
-                              }
-                            },
-                          ),
-                          const SizedBox(
-                            height: 25,
-                          ),
-                          SizedBox(
-                            height: 60,
-                            child: ElevatedButton(
-                                onPressed: () {
-                                  if (_formKey.currentState!.validate()) {
-                                    model.signIn(
-                                        email: _emailController.text,
-                                        password: _passwordController.text,
-                                        onSuccess: onSuccess,
-                                        onError: onError);
-                                  }
-                                },
-                                child: const Text(
-                                  'Entrar',
-                                  style: TextStyle(fontSize: 25),
-                                ),
-                                style: ButtonStyle(
-                                    shape: MaterialStateProperty.all<
-                                            RoundedRectangleBorder>(
-                                        RoundedRectangleBorder(
-                                            borderRadius:
-                                                BorderRadius.circular(16.0),
-                                            side: const BorderSide(
-                                                color: Colors.black))))),
-                          ),
-                        ],
+                            const Text(
+                              'Controle de Pedidos',
+                              style: TextStyle(fontSize: 30, color: Colors.white),
+                              textAlign: TextAlign.center,
+                            ),
+
+                            /*const Icon(
+                              Icons.water_damage_outlined,
+                              size: 150,
+                              color: Colors.white,
+                            ),*/
+
+                            const SizedBox(
+                              height: 15,
+                            ),
+                            TextFormField(
+                              controller: _emailController,
+                              decoration: const InputDecoration(
+                                hintText: 'Email',
+                                hintStyle: TextStyle(color: Colors.white),
+                              ),
+                              autofillHints: const [AutofillHints.email],
+                              style: const TextStyle(color: Colors.white),
+                              textInputAction: TextInputAction.next,
+                              validator: (text) {
+                                bool regValida = RegExp(
+                                        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                                    .hasMatch(text!);
+                                if (text.isEmpty || !regValida) {
+                                  return 'Email Inválido';
+                                }
+                              },
+                            ),
+                            const SizedBox(
+                              height: 30,
+                            ),
+                            TextFormField(
+                              controller: _passwordController,
+                              decoration: const InputDecoration(
+                                hintText: 'Senha',
+                                hintStyle: TextStyle(color: Colors.white),
+                              ),
+                              autofillHints: const [AutofillHints.email],
+                              style: const TextStyle(color: Colors.white),
+                              obscureText: true,
+                              validator: (text) {
+                                if (text!.isEmpty) {
+                                  return 'Preencha o campo';
+                                } else if (text.length < 6) {
+                                  return 'A senha deve conter no minimo 6 caracteres';
+                                }
+                              },
+                              onFieldSubmitted: (e) {
+                                if (_formKey.currentState!.validate()) {
+                                  model.signIn(
+                                      email: _emailController.text,
+                                      password: _passwordController.text,
+                                      onSuccess: onSuccess,
+                                      onError: onError);
+                                }
+                              },
+                            ),
+                            const SizedBox(
+                              height: 25,
+                            ),
+                            SizedBox(
+                              height: 60,
+                              child: ElevatedButton(
+                                  onPressed: () {
+                                    if (_formKey.currentState!.validate()) {
+                                      model.signIn(
+                                          email: _emailController.text,
+                                          password: _passwordController.text,
+                                          onSuccess: onSuccess,
+                                          onError: onError);
+                                    }
+                                  },
+                                  child: const Text(
+                                    'Entrar',
+                                    style: TextStyle(fontSize: 25),
+                                  ),
+                                  style: ButtonStyle(
+                                      shape: MaterialStateProperty.all<
+                                              RoundedRectangleBorder>(
+                                          RoundedRectangleBorder(
+                                              borderRadius:
+                                                  BorderRadius.circular(16.0),
+                                              side: const BorderSide(
+                                                  color: Colors.black))))),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   ),
