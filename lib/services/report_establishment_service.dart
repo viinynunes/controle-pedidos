51d630aa440ba806369321d4ff36f5622fe34fb2
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-05-04 16:51:08 -0300

    Changed the code to support more then two items by different providers

--- lib/services/report_establishment_service.dart
+++ lib/services/report_establishment_service.dart
@@ -1,3 +1,6 @@
+import 'package:controle_pedidos/data/establishment_data.dart';
+import 'package:controle_pedidos/data/product_data.dart';
+import 'package:controle_pedidos/data/provider_data.dart';
 import 'package:controle_pedidos/data/stock_data.dart';
 import 'package:controle_pedidos/model/stock_model.dart';
 import 'package:flutter/material.dart';
@@ -13,21 +16,38 @@ class ReportEstablishmentService {
     for (var item in stockList) {
       if (stockDataList.contains(item)) {
         var list = stockDataList
-            .where((element) => element.product.id == item.product.id).toList();
+            .where((element) => element.product.id == item.product.id)
+            .toList();
         late StockData e;
-        if (list.length > 1) {
-          e = list.singleWhere((element) =>
-              element.product.id == item.product.id &&
-              element.product.provider.id == item.product.provider.id);
-        } else {
+        if (list.length == 1) {
           e = list
               .singleWhere((element) => element.product.id == item.product.id);
+        } else if (list.length > 1) {
+          for (var aux in list) {
+            if (aux.product.provider.id == item.product.provider.id) {
+              e = list.singleWhere((element) =>
+                  element.product.id == item.product.id &&
+                  element.product.provider.id == item.product.provider.id);
+              break;
+            } else {
+              final auxProd = ProductData();
+              auxProd.provider = ProviderData();
+              auxProd.provider.name = '';
+              auxProd.provider.location = '';
+              auxProd.provider.registrationDate = DateTime.now();
+              auxProd.provider.enabled = true;
+              auxProd.provider.establishment = EstablishmentData();
+              e = StockData(0, 0, DateTime.now(), auxProd);
+            }
+          }
         }
 
         if (e.product.provider == item.product.provider) {
           item.total += e.total;
           item.totalOrdered += e.totalOrdered;
-          stockDataList.removeWhere((element) => element.product.id == e.product.id && element.product.provider.id == e.product.provider.id);
+          stockDataList.removeWhere((element) =>
+              element.product.id == e.product.id &&
+              element.product.provider.id == e.product.provider.id);
         }
       }
       stockDataList.add(item);
