285fec17dd18c4952bef8fae27a51dd87a48f570
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-10-28 17:12:57 -0300

    Verifying if map['registrationDate'] is timestamp

--- lib/src/modules/establishment/infra/models/establish_model.dart
+++ lib/src/modules/establishment/infra/models/establish_model.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:controle_pedidos/src/modules/establishment/domain/entities/establishment.dart';
 
 import '../../../core/helpers.dart';
@@ -19,8 +20,9 @@ class EstablishmentModel extends Establishment {
   EstablishmentModel.fromMap({required Map<String, dynamic> map})
       : super(
           id: map['id'],
-          registrationDate:
-              Helpers.convertTimestampToDateTime(map['registrationDate']),
+          registrationDate: map['registrationDate'] is Timestamp
+              ? Helpers.convertTimestampToDateTime(map['registrationDate'])
+              : map['registrationDate'],
           name: map['name'],
           enabled: map['enabled'],
         );
