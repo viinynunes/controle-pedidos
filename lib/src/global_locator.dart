35cf7bdc4bb285c589e6771b28859705ac8ef772
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-10-27 22:30:34 -0300

    setUp establishment locator

--- lib/src/global_locator.dart
+++ lib/src/global_locator.dart
@@ -1,8 +1,10 @@
 import 'package:controle_pedidos/src/modules/client/client_locator.dart';
+import 'package:controle_pedidos/src/modules/establishment/establishment_locator.dart';
 import 'package:get_it/get_it.dart';
 
 final locator = GetIt.instance;
 
 void initGlobalServiceLocator() {
   setUpClientLocator();
+  setUpEstablishmentLocator();
 }
