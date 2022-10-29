03bee3d18b07f644ba9742ca7700a3d989642272
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-10-29 15:22:55 -0300

    Custom TextField to use in all registrations page

---
+++ lib/src/modules/core/widgets/custom_text_form_field.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class CustomTextFormField extends StatefulWidget {
+  const CustomTextFormField(
+      {Key? key,
+      required this.controller,
+      required this.label,
+      this.focus,
+      this.validator,
+      this.textInputType})
+      : super(key: key);
+
+  final TextEditingController controller;
+  final String label;
+  final FocusNode? focus;
+  final String? Function(String?)? validator;
+  final TextInputType? textInputType;
+
+  @override
+  State<CustomTextFormField> createState() => _CustomTextFormFieldState();
+}
+
+class _CustomTextFormFieldState extends State<CustomTextFormField> {
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: widget.controller,
+      focusNode: widget.focus,
+      decoration: InputDecoration(
+        labelText: widget.label,
+        labelStyle: Theme.of(context).textTheme.bodyMedium,
+      ),
+      style: Theme.of(context).textTheme.bodyMedium,
+      validator: widget.validator,
+      textInputAction: TextInputAction.next,
+      keyboardType: widget.textInputType,
+    );
+  }
+}
