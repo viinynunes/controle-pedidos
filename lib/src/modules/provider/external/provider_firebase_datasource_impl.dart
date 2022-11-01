7f08846395ba864da59aa2e45dfe4cb55e1d8f26
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-11-01 11:18:21 -0300

    GetProviderListByEnabled Implemented and awaiting firebase to return an entity on create and update

--- lib/src/modules/provider/external/provider_firebase_datasource_impl.dart
+++ lib/src/modules/provider/external/provider_firebase_datasource_impl.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:controle_pedidos/src/modules/firebase_helper.dart';
-import 'package:controle_pedidos/src/modules/provider/domain/entities/provider.dart';
 import 'package:controle_pedidos/src/modules/provider/infra/datasources/i_provider_datasource.dart';
 import 'package:controle_pedidos/src/modules/provider/infra/models/provider_model.dart';
 
@@ -9,30 +8,52 @@ class ProviderFirebaseDatasourceImpl implements IProviderDatasource {
       FirebaseHelper.firebaseCollection.collection('provider');
 
   @override
-  Future<bool> createProvider(ProviderModel provider) async {
-    _providerCollection.add({}).then((value) {
-      provider.id = value.id;
-      _providerCollection.doc(provider.id).update(provider.toMap());
-    }).catchError(
+  Future<ProviderModel> createProvider(ProviderModel provider) async {
+    final result = await _providerCollection.add({}).catchError(
         (e) => throw FirebaseException(plugin: 'CREATE PROVIDER ERROR'));
 
-    return true;
+    provider.id = result.id;
+    return await updateProvider(provider);
   }
 
   @override
-  Future<bool> updateProvider(ProviderModel provider) async {
-    _providerCollection.doc(provider.id).update(provider.toMap()).catchError(
-        (e) => throw FirebaseException(plugin: 'UPDATE PROVIDER ERROR'));
+  Future<ProviderModel> updateProvider(ProviderModel provider) async {
+    await _providerCollection
+        .doc(provider.id)
+        .update(provider.toMap())
+        .catchError(
+            (e) => throw FirebaseException(plugin: 'UPDATE PROVIDER ERROR'));
 
-    return true;
+    return provider;
   }
 
   @override
-  Future<List<Provider>> getProviderList() async {
+  Future<List<ProviderModel>> getProviderListByEnabled() async {
     List<ProviderModel> providerList = [];
 
-    final snap =
-        await _providerCollection.orderBy('name', descending: false).get();
+    final snap = await _providerCollection
+        .where('enabled', isEqualTo: true)
+        .orderBy('name', descending: false)
+        .get()
+        .catchError(
+            (e) => throw FirebaseException(plugin: 'GET PROVIDER ERROR'));
+
+    for (var i in snap.docs) {
+      providerList.add(ProviderModel.fromMap(map: i.data()));
+    }
+
+    return providerList;
+  }
+
+  @override
+  Future<List<ProviderModel>> getProviderList() async {
+    List<ProviderModel> providerList = [];
+
+    final snap = await _providerCollection
+        .orderBy('name', descending: false)
+        .get()
+        .catchError(
+            (e) => throw FirebaseException(plugin: 'GET PROVIDER ERROR'));
 
     for (var i in snap.docs) {
       providerList.add(ProviderModel.fromMap(map: i.data()));
