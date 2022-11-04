2578b62437d631c0a5102878bade055be4527875
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-11-04 15:03:09 -0300

    setting up order locator

--- lib/src/global_locator.dart
+++ lib/src/global_locator.dart
@@ -1,13 +1,11 @@
 import 'package:controle_pedidos/src/modules/client/client_locator.dart';
 import 'package:controle_pedidos/src/modules/establishment/establishment_locator.dart';
-import 'package:get_it/get_it.dart';
 
 import 'modules/core/drawer/drawer_locator.dart';
 import 'modules/core/widgets/widgets_locator.dart';
+import 'modules/order/order_locator.dart';
 import 'modules/product/product_locator.dart';
 import 'modules/provider/provider_locator.dart';
-
-final locator = GetIt.instance;
 
 Future initGlobalServiceLocator() async {
   setUpWidgetsLocator();
@@ -16,4 +14,5 @@ Future initGlobalServiceLocator() async {
   setUpEstablishmentLocator();
   setUpProviderLocator();
   setUpProductLocator();
+  setUpOrderLocator();
 }
