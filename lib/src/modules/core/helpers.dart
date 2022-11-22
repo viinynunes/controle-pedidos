836c7cc8ab95ddd5530c75ed7e657e6297cc760e
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-11-21 21:34:30 -0300

    Comparing two dates

--- lib/src/modules/core/helpers.dart
+++ lib/src/modules/core/helpers.dart
@@ -4,4 +4,12 @@ class Helpers {
   static DateTime convertTimestampToDateTime(Timestamp timestamp) {
     return DateTime.parse(timestamp.toDate().toString());
   }
+
+  static bool compareOnlyDateFromDateTime(
+      DateTime firstDate, DateTime lastLate) {
+    firstDate = DateTime(firstDate.year, firstDate.month, firstDate.day);
+    lastLate = DateTime(lastLate.year, lastLate.month, lastLate.day);
+
+    return firstDate == lastLate;
+  }
 }
