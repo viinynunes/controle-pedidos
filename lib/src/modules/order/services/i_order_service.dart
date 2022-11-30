a95d64f2f4020c2ec88b5bd528b5bdb3f7dd90c5
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-11-30 16:14:28 -0300

    mergeOrderListByClient function implemented

--- lib/src/modules/order/services/i_order_service.dart
+++ lib/src/modules/order/services/i_order_service.dart
@@ -1,7 +1,11 @@
-import 'package:controle_pedidos/src/domain/entities/order.dart';
+import 'package:controle_pedidos/src/domain/entities/order.dart'as o;
+import 'package:controle_pedidos/src/modules/order/errors/order_error.dart';
+import 'package:dartz/dartz.dart';
 
 abstract class IOrderService {
-  void sortOrderListByRegistrationHour(List<Order> orderList);
+  void sortOrderListByRegistrationHour(List<o.Order> orderList);
 
-  void sortOrderListByClientName(List<Order> orderList);
+  void sortOrderListByClientName(List<o.Order> orderList);
+
+  Either<OrderError, List<o.Order>> mergeOrderListByClient(List<o.Order> orderList);
 }
--- lib/src/modules/order/services/impl/order_service_impl.dart
+++ lib/src/modules/order/services/impl/order_service_impl.dart
@@ -1,24 +1,97 @@
-import 'package:controle_pedidos/src/domain/entities/order.dart';
+import 'package:controle_pedidos/src/domain/entities/order.dart' as o;
+import 'package:controle_pedidos/src/domain/entities/order_item.dart';
+import 'package:controle_pedidos/src/modules/order/errors/order_error.dart';
+import 'package:dartz/dartz.dart';
 
 import '../i_order_service.dart';
 
 class OrderServiceImpl implements IOrderService {
   @override
-  void sortOrderListByRegistrationHour(List<Order> orderList) {
+  void sortOrderListByRegistrationHour(List<o.Order> orderList) {
     orderList.sort((a, b) => b.registrationHour.compareTo(a.registrationHour));
   }
 
   @override
-  void sortOrderListByClientName(List<Order> orderList) {
+  void sortOrderListByClientName(List<o.Order> orderList) {
     orderList.sort((a, b) {
-      final compare =  a.client.name.toLowerCase().compareTo(b.client.name.toLowerCase());
+      final compare =
+          a.client.name.toLowerCase().compareTo(b.client.name.toLowerCase());
 
-
-      if(compare == 0){
+      if (compare == 0) {
         return a.registrationHour.compareTo(b.registrationHour);
       }
 
       return compare;
+    });
+  }
+
+  @override
+  Either<OrderError, List<o.Order>> mergeOrderListByClient(
+      List<o.Order> orderList) {
+    List<o.Order> mergedList = [];
+    o.Order? previousOrder;
+
+    sortOrderListByClientName(orderList);
+
+    for (var actualOrder in orderList) {
+      if (previousOrder != actualOrder) {
+        sortOrderItems(actualOrder.orderItemList);
+        mergedList.add(actualOrder);
+        previousOrder = actualOrder;
+      } else {
+        for (var actualItem in actualOrder.orderItemList) {
+          ///previous order not contain the item
+          if (previousOrder != null &&
+              !previousOrder.orderItemList.contains(actualItem)) {
+            previousOrder.orderItemList.add(actualItem);
+          } else {
+            try {
+              var previousItem = previousOrder!.orderItemList
+                  .singleWhere((element) => element == actualItem);
+
+              if (previousItem.note.isNotEmpty && actualItem.note.isNotEmpty) {
+                previousOrder.orderItemList.add(actualItem);
+              }
+
+              if (previousItem.note.isEmpty && actualItem.note.isEmpty) {
+                previousItem.quantity += actualItem.quantity;
+              }
+
+              if (previousItem.note.isEmpty && actualItem.note.isNotEmpty ||
+                  previousItem.note.isNotEmpty && actualItem.note.isEmpty) {
+                previousItem.quantity += actualItem.quantity;
+                previousItem.note = actualItem.note;
+              }
+            } on StateError {
+              return Left(OrderError('Mais de um item encontrado'));
+            } catch (e) {
+              return Left(OrderError(e.toString()));
+            }
+          }
+        }
+      }
+
+      ///previous order and actual order have the same client
+
+    }
+
+    return Right(mergedList);
+  }
+
+  void sortOrderItems(List<OrderItem> orderItemList) {
+    orderItemList.sort((a, b) {
+      int compare = a.product.category
+          .toLowerCase()
+          .compareTo(b.product.category.toLowerCase());
+
+      if (compare == 0) {
+        var name1 = a.product.name.toLowerCase();
+        var name2 = b.product.name.toLowerCase();
+
+        return name1.compareTo(name2);
+      } else {
+        return compare;
+      }
     });
   }
 }
