34a8c3cae02839a6201acd99d1abbf33944645f6
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-11-30 16:16:09 -0300

    First layout

---
+++ lib/src/modules/order/presenter/pages/android/reports/android_order_report_page.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:get_it/get_it.dart';
+
+import '../../../stores/order_report_controller.dart';
+import '../tiles/android_order_list_tile.dart';
+
+class AndroidOrderReportPage extends StatefulWidget {
+  const AndroidOrderReportPage({Key? key}) : super(key: key);
+
+  @override
+  State<AndroidOrderReportPage> createState() => _AndroidOrderReportPageState();
+}
+
+class _AndroidOrderReportPageState extends State<AndroidOrderReportPage> {
+  final controller = GetIt.I.get<OrderReportController>();
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Relatório de Pedidos'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              controller.mergeOrderList();
+            },
+            icon: const Icon(Icons.share),
+          ),
+        ],
+      ),
+      body: SafeArea(
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                Center(
+                  child: ElevatedButton(
+                    onLongPress: controller.resetDateRange,
+                    onPressed: () async {
+                      final result = await showDateRangePicker(
+                          context: context,
+                          initialDateRange: DateTimeRange(
+                              start: controller.iniDate,
+                              end: controller.endDate),
+                          firstDate: DateTime(2020),
+                          lastDate: DateTime(2200));
+
+                      if (result != null) {
+                        controller.setDateRange(result.start, result.end);
+                      }
+                    },
+                    child: Observer(
+                      builder: (context) {
+                        return Text(controller.dateRange);
+                      },
+                    ),
+                  ),
+                ),
+                const Divider(),
+                Expanded(
+                  child: Observer(
+                    builder: (_) {
+                      if (controller.loading) {
+                        return const Center(child: CircularProgressIndicator());
+                      }
+
+                      final orderList = controller.orderList;
+
+                      return orderList.isNotEmpty
+                          ? ListView.builder(
+                              itemCount: orderList.length,
+                              itemBuilder: (_, index) {
+                                final order = orderList[index];
+                                return AndroidOrderListTile(
+                                  order: order,
+                                  onTap: () {
+                                    showModalBottomSheet(context: context, builder: (_) => Container());
+                                  },
+                                  onDisable: () {},
+                                );
+                              },
+                            )
+                          : const Center(
+                              child: Text('Nenhum pedido encontrado'),
+                            );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
