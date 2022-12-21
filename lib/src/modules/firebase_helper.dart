8d04e8c1c505d2b89b70648bc0b7fddd69398639
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-12-21 15:23:20 -0300

    Storing all collections

--- lib/src/modules/firebase_helper.dart
+++ lib/src/modules/firebase_helper.dart
@@ -1,5 +1,14 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class FirebaseHelper {
-  static final firebaseCollection = FirebaseFirestore.instance.collection('company').doc('JifXJRp03H9Sgj1pNHzQ');
+  static final firebaseDb = FirebaseFirestore.instance;
+  static final firebaseCompany = firebaseDb.collection('company').doc('JifXJRp03H9Sgj1pNHzQ');
+  static final clientCollection = firebaseCompany.collection('client');
+  static final productCollection = firebaseCompany.collection('product');
+  static final providerCollection = firebaseCompany.collection('provider');
+  static final establishmentCollection = firebaseCompany.collection('establishment');
+  static final stockCollection = firebaseCompany.collection('stock');
+  static final orderCollection = firebaseCompany.collection('order');
+
+
 }
