4feb75125d4df662dd10942a3507b3017bc2438a
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-12-26 16:18:37 -0300

    Added Firebase Auth and user collection

--- lib/src/modules/firebase_helper.dart
+++ lib/src/modules/firebase_helper.dart
@@ -1,8 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class FirebaseHelper {
   static final firebaseDb = FirebaseFirestore.instance;
+  static final firebaseAuth = FirebaseAuth.instance;
   static final firebaseCompany = firebaseDb.collection('company').doc('JifXJRp03H9Sgj1pNHzQ');
+  static final userCollection = firebaseCompany.collection('user');
   static final clientCollection = firebaseCompany.collection('client');
   static final productCollection = firebaseCompany.collection('product');
   static final providerCollection = firebaseCompany.collection('provider');
