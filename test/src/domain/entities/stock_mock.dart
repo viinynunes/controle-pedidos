55a8b24d0a0ccee1e82896c0b13953d7e48eda32
Vinicius Nunes <viiny_nunes@hotmail.com>
2023-01-16 15:42:18 -0300

    Class with Mocked data of Stock

--- test/src/domain/entities/stock_mock.dart
+++ test/src/domain/entities/stock_mock.dart
@@ -1,4 +1,8 @@
 import 'package:controle_pedidos/src/domain/entities/product.dart';
+import 'package:controle_pedidos/src/domain/models/product_model.dart';
+import 'package:controle_pedidos/src/domain/models/stock_model.dart';
+
+import 'product_mock.dart';
 
 class StockMock {
   static String getStockCodeFromProduct(
@@ -6,5 +10,24 @@ class StockMock {
     return product.id +
         product.provider.id +
         DateTime(date.year, date.month, date.day).toString();
+  }
+
+  static StockModel getOneStock(
+      {String id = 'stockID-001',
+      String? code,
+      int total = 2,
+      int totalOrdered = 3,
+      DateTime? registrationDate,
+      ProductModel? product}) {
+    return StockModel(
+        id: id,
+        code: code ??
+            getStockCodeFromProduct(
+                product: product ?? ProductMock.getOneProduct(),
+                date: registrationDate ?? DateTime.now()),
+        total: total,
+        totalOrdered: totalOrdered,
+        registrationDate: registrationDate ?? DateTime.now(),
+        product: product ?? ProductMock.getOneProduct());
   }
 }
