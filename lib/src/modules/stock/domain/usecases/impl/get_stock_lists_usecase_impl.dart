93aaf5b060c73fd10edb6cde3bf7f10e54508bfd
Vinicius Nunes <viiny_nunes@hotmail.com>
2023-01-23 18:05:20 -0300

    Implementation of Get Stock Lists Usecase

---
+++ lib/src/modules/stock/domain/usecases/impl/get_stock_lists_usecase_impl.dart
@@ -0,0 +1,43 @@
+import 'package:controle_pedidos/src/domain/entities/provider.dart';
+
+import 'package:controle_pedidos/src/domain/entities/stock.dart';
+import 'package:controle_pedidos/src/modules/stock/domain/repositories/i_new_stock_repository.dart';
+
+import 'package:controle_pedidos/src/modules/stock/errors/stock_error.dart';
+
+import 'package:dartz/dartz.dart';
+
+import '../get_stock_lists_usecase.dart';
+
+class GetStockListsUsecaseImpl implements GetStockListsUsecase {
+  final INewStockRepository _repository;
+
+  GetStockListsUsecaseImpl(this._repository);
+
+  @override
+  Future<Either<StockError, Set<Provider>>> getProviderListByStockBetweenDates(
+      {required DateTime iniDate, required DateTime endDate}) {
+    return _repository.getProviderListByStockBetweenDates(
+        iniDate: iniDate, endDate: endDate);
+  }
+
+  @override
+  Future<Either<StockError, List<Stock>>> getStockListBetweenDates(
+      {required DateTime iniDate, required DateTime endDate}) {
+    return _repository.getStockListBetweenDates(
+        iniDate: iniDate, endDate: endDate);
+  }
+
+  @override
+  Future<Either<StockError, List<Stock>>> getStockListByProviderBetweenDates(
+      {required Provider provider,
+      required DateTime iniDate,
+      required DateTime endDate}) async {
+    if (provider.id.isEmpty) {
+      return Left(StockError('Provider ID cannot be empty'));
+    }
+
+    return _repository.getStockListByProviderBetweenDates(
+        provider: provider, iniDate: iniDate, endDate: endDate);
+  }
+}
