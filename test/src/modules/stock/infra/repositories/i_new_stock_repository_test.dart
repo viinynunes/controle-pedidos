3596eb534e28b53a3d66bfe8f662ade70b07a4cb
Vinicius Nunes <viiny_nunes@hotmail.com>
2023-01-23 14:55:04 -0300

    Tests to change stock date

---
+++ test/src/modules/stock/infra/repositories/i_new_stock_repository_test.dart
@@ -0,0 +1,139 @@
+import 'package:controle_pedidos/src/domain/entities/stock.dart';
+import 'package:controle_pedidos/src/modules/stock/errors/stock_error.dart';
+import 'package:controle_pedidos/src/modules/stock/infra/repositories/new_stock_repository_impl.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+import '../../../../domain/entities/product_mock.dart';
+import '../../../../domain/entities/stock_mock.dart';
+import '../../external/mocks/mock_datasource.mocks.dart';
+
+main() {
+  final datasource = MockINewStockDatasource();
+  final repository = NewStockRepositoryImpl(datasource);
+  final product = ProductMock.getOneProduct();
+  final date = DateTime.now();
+  const stockID = 'stockID01';
+  String stockCode = product.id +
+      product.provider.id +
+      DateTime(date.year, date.month, date.day).toString();
+  const stockTotal = 1;
+  const stockTotalOrdered = 1;
+  const stockTotalFromCode = 2;
+  const stockTotalOrderedFromCode = 3;
+  final defaultStock =
+      StockMock.getOneStock(id: stockID, code: stockCode, total: stockTotal);
+
+  setUp(() {
+    when(datasource.getStockById(id: anyNamed('id'))).thenAnswer((_) async =>
+        StockMock.getOneStock(
+            id: stockID,
+            code: stockCode,
+            total: stockTotal,
+            totalOrdered: stockTotalOrdered));
+
+    when(datasource.getStockByCode(code: anyNamed('code'))).thenAnswer(
+        (_) async => StockMock.getOneStock(
+            code: stockCode,
+            total: stockTotalFromCode,
+            totalOrdered: stockTotalOrderedFromCode));
+
+    when(datasource.deleteStock(stock: defaultStock)).thenAnswer(
+        (Invocation invocation) async =>
+            invocation.namedArguments[const Symbol('stock')]);
+
+    when(datasource.createStock(
+            stock: defaultStock, stockID: anyNamed('stockID')))
+        .thenAnswer((Invocation invocation) async =>
+            invocation.namedArguments[const Symbol('stock')]);
+
+    when(datasource.updateStock(stock: defaultStock)).thenAnswer(
+        (Invocation invocation) async =>
+            invocation.namedArguments[const Symbol('stock')]);
+  });
+
+  group('tests to change stock date in repository', () {
+    test('have to catch an exception when getStockById don\'t found a stock',
+        () async {
+      when(datasource.getStockById(id: anyNamed('id')))
+          .thenThrow(() => throw StockError('Stock not found'));
+
+      final result =
+          await repository.changeStockDate(stockId: stockID, newDate: date);
+
+      expect(result, isA<Left>());
+      expect(result.fold(id, id), isA<Exception>());
+    });
+
+    test('have to catch an exception when delete stock fail', () async {
+      when(datasource.deleteStock(stock: defaultStock))
+          .thenThrow(() => throw StockError('Delete Stock Fail'));
+
+      final result =
+          await repository.changeStockDate(stockId: stockID, newDate: date);
+
+      expect(result, isA<Left>());
+      expect(result.fold(id, id), isA<Exception>());
+    });
+
+    test('have to catch an exception when create stock fail', () async {
+      when(datasource.createStock(
+              stock: defaultStock, stockID: anyNamed('stockID')))
+          .thenThrow(() => throw StockError('Create Stock Fail'));
+
+      when(datasource.getStockByCode(code: anyNamed('code')))
+          .thenAnswer((_) async => null);
+
+      final result =
+          await repository.changeStockDate(stockId: stockID, newDate: date);
+
+      expect(result, isA<Left>());
+      expect(result.fold(id, id), isA<Exception>());
+    });
+
+    test('have to catch an exception when update stock fail', () async {
+      when(datasource.updateStock(stock: defaultStock))
+          .thenThrow(() => throw StockError('Delete Stock Fail'));
+
+      final result =
+          await repository.changeStockDate(stockId: stockID, newDate: date);
+
+      expect(result, isA<Left>());
+      expect(result.fold(id, id), isA<Exception>());
+    });
+
+    test('have to create a new stock when getStockByCode return null',
+        () async {
+      when(datasource.getStockByCode(code: anyNamed('code')))
+          .thenAnswer((_) async => null);
+
+      final result = await repository.changeStockDate(
+          stockId: stockID, newDate: date.add(const Duration(days: 1)));
+
+      expect(result, isA<Right>());
+      expect(result.fold(id, id), isA<Stock>());
+      expect(result.fold((l) => null, (r) => r.total), stockTotal);
+      expect(
+          result.fold((l) => null, (r) => r.totalOrdered), stockTotalOrdered);
+    });
+
+    test('have to update the stock when getStockByCode return a stock from DB',
+        () async {
+      when(datasource.getStockByCode(code: anyNamed('code'))).thenAnswer(
+          (_) async => StockMock.getOneStock(
+              total: stockTotalFromCode,
+              totalOrdered: stockTotalOrderedFromCode));
+
+      final result = await repository.changeStockDate(
+          stockId: stockID, newDate: date.add(const Duration(days: 1)));
+
+      expect(result, isA<Right>());
+      expect(result.fold(id, id), isA<Stock>());
+      expect(result.fold((l) => null, (r) => r.total),
+          stockTotal + stockTotalFromCode);
+      expect(result.fold((l) => null, (r) => r.totalOrdered),
+          stockTotal + stockTotalOrderedFromCode);
+    });
+  });
+}
