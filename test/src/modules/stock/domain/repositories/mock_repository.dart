2f06ae3afcaa661e2f835307eb0307521f6908af
Vinicius Nunes <viiny_nunes@hotmail.com>
2023-01-23 16:48:36 -0300

    Mocking classes

---
+++ test/src/modules/stock/domain/repositories/mock_repository.dart
@@ -0,0 +1,5 @@
+import 'package:controle_pedidos/src/modules/stock/domain/repositories/i_new_stock_repository.dart';
+import 'package:mockito/annotations.dart';
+
+@GenerateMocks([INewStockRepository])
+main() {}
