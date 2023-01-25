422ede7acb6114c0b87cf04bf8e7afec47d96fa9
Vinicius Nunes <viiny_nunes@hotmail.com>
2023-01-25 11:19:38 -0300

    New contracts to stock datasource

--- lib/src/modules/stock/infra/datasources/i_new_stock_datasource.dart
+++ lib/src/modules/stock/infra/datasources/i_new_stock_datasource.dart
@@ -1,29 +1,16 @@
-import '../../../../domain/models/product_model.dart';
 import '../../../../domain/models/provider_model.dart';
 import '../../../../domain/models/stock_model.dart';
 
 abstract class INewStockDatasource {
-  Future<StockModel> increaseTotalFromStock(
-      {required ProductModel product,
-      required DateTime date,
-      required int increaseQuantity});
+  Future<StockModel> createStock({required StockModel stock, String stockID = ''});
 
-  Future<StockModel> decreaseTotalFromStock(
-      {required ProductModel product,
-      required DateTime date,
-      required int decreaseQuantity,
-      bool deleteIfEmpty = false});
+  Future<StockModel> updateStock({required StockModel stock});
 
-  Future<StockModel> increaseTotalOrderedFromStock(
-      {required String stockID, required int increaseQuantity});
+  Future<StockModel> deleteStock({required StockModel stock});
 
-  Future<StockModel> decreaseTotalOrderedFromStock(
-      {required String stockID, required int decreaseQuantity});
+  Future<StockModel?> getStockByCode({required String code});
 
-  Future<StockModel> deleteStock(StockModel stock);
-
-  Future<StockModel> changeStockDate(
-      {required String stockId, required DateTime newDate});
+  Future<StockModel> getStockById({required String id});
 
   Future<Set<ProviderModel>> getProviderListByStockBetweenDates(
       {required DateTime iniDate, required DateTime endDate});
